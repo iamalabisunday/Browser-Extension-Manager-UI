53bc6d73a14283c5595d37b3558af5b91bb7abbd
Sunday Alabi <iamalabisunday@gmail.com>
2025-07-22 04:16:00 -0700

    first commit

--- src/BrowserExtension.jsx
+++ src/BrowserExtension.jsx
@@ -1,27 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./images/logo.svg";
 import iconMoon from "./images/icon-moon.svg";
 import iconSun from "./images/icon-sun.svg";
 
 export default function BrowserExtension() {
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    }
+  }, [darkMode]);
+
+  const togglehandle = (e) => {
+    e.preventDefault();
+    setDarkMode((prev) => !prev);
+  };
 
   return (
     <>
-      <div
-        className={`w-full h-full ${
-          isDarkTheme
-            ? `bg-[var-(--colorNeutral-8)]`
-            : `bg-[var-(--colorNeutral-2)`
-        }`}
-      >
-        <main className="container w-[90%]  mx-auto">
+      <div className="w-full h-full bg-[var(--background)]">
+        <main className="container w-[90%] mx-auto">
           <header className="w-full h-auto mt-8 flex items-start justify-between">
             <img src={logo} alt="logo" />
             <img
-              src={isDarkTheme ? iconMoon : iconSun}
-              alt="theme"
-              onClick={() => setIsDarkTheme(!isDarkTheme)}
+              src={darkMode ? iconMoon : iconSun}
+              alt="Icon switching mode between Sun/Moon"
+              onClick={togglehandle}
+              className="cursor-pointer"
             />
           </header>
         </main>
