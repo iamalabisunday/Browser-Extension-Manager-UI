d56741838ce73cce90dab53e5ccb32f875a67520
Sunday Alabi <iamalabisunday@gmail.com>
2025-07-23 09:22:26 -0700

    first commit

--- src/BrowserExtension.jsx
+++ src/BrowserExtension.jsx
@@ -8,6 +8,15 @@ import ExtensionButton from "./ExtensionButton.jsx";
 import DevImage from "./images/logo-devlens.svg";
 import SpyImage from "./images/logo-style-spy.svg";
 import SpeedImage from "./images/logo-speed-boost.svg";
+import JSONWizard from "./images/logo-json-wizard.svg";
+import TabMaster from "./images/logo-tab-master-pro.svg";
+import ViewportBuddy from "./images/logo-viewport-buddy.svg";
+import MarkupNotes from "./images/logo-markup-notes.svg";
+import GridGuides from "./images/logo-grid-guides.svg";
+import PalettePicker from "./images/logo-palette-picker.svg";
+import LinkChecker from "./images/logo-link-checker.svg";
+import DOMSnapshot from "./images/logo-dom-snapshot.svg";
+import ConsolePlus from "./images/logo-console-plus.svg";
 
 export default function BrowserExtension() {
   // Theme toggle between light and dark mode
@@ -54,6 +63,74 @@ export default function BrowserExtension() {
       paragraph: "Optimizes browser resource usage to accelerate page loading.",
       active: true,
     },
+    {
+      id: 4,
+      icon: JSONWizard,
+      title: "JSONWizard",
+      paragraph:
+        "Formats, validates, and prettifies JSON responses in browser.",
+      active: true,
+    },
+    {
+      id: 5,
+      icon: TabMaster,
+      title: "TabMaster Pro",
+      paragraph: "Organizes browser tabs into groups and sessions.",
+      active: true,
+    },
+    {
+      id: 6,
+      icon: ViewportBuddy,
+      title: "ViewportBuddy",
+      paragraph:
+        "Simulates various screen resolutions directly within the browser.",
+      active: true,
+    },
+    {
+      id: 7,
+      icon: MarkupNotes,
+      title: "Markup Notes",
+      paragraph:
+        "Enables annotation and notes directly onto webpages for collaborative debugging.",
+      active: true,
+    },
+    {
+      id: 8,
+      icon: GridGuides,
+      title: "GridGuides",
+      paragraph:
+        "Overlay customizable grids and alignment guides on any webpage.",
+      active: true,
+    },
+    {
+      id: 9,
+      icon: PalettePicker,
+      title: "Palette Picker",
+      paragraph: "Instantly extracts color palettes from any webpage.",
+      active: true,
+    },
+    {
+      id: 10,
+      icon: LinkChecker,
+      title: "LinkChecker",
+      paragraph: "Scans and highlights broken links on any page.",
+      active: true,
+    },
+    {
+      id: 11,
+      icon: DOMSnapshot,
+      title: "DOM Snapshot",
+      paragraph: "Capture and export DOM structures quickly",
+      active: true,
+    },
+    {
+      id: 12,
+      icon: ConsolePlus,
+      title: "ConsolePlus",
+      paragraph:
+        "Enhanced developer console with advanced flitering and logging.",
+      active: true,
+    },
   ]);
 
   // Filter state: All, Active, Inactive
@@ -79,7 +156,7 @@ export default function BrowserExtension() {
   };
 
   return (
-    <div className="w-screen h-screen bg-[var(--color-background)] text-[var(--color-border)] transition-colors duration-300">
+    <div className="w-full min-h-screen pb-8 bg-[var(--color-background)] text-[var(--color-border)] transition-colors duration-300">
       <main className="container w-[85%] mx-auto">
         {/* Navbar */}
         <header className="w-full pt-6">
